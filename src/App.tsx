5450b4b612592219bf651cc1c5b446532fd8810a
xic71450-sketch <xic71450@gmail.com>
2025-10-18 01:54:03 +0000

    fix: update App component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AuthGate from "./components/AuthGate";
 import Home from "./components/Home";
